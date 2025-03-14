094947e66e0df215026326ae62cd11ad7d2edb1d
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-03-15 02:05:15 +0530

    A testing of ShadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import { ConfettiButton } from "@/components/magicui/confetti";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Welcome to my portfolio.</h1>
-      <p>- Himanshu</p>
+    <div className="p-4 mx-[520px]">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <div className="relative mt-20 flex justify-center">
+        <ConfettiButton>Confetti 🎉</ConfettiButton>
+      </div>
     </div>
   );
 }
